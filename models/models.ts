6cac60e8dcc8da8a347ca3f82a4cad9d04b5b592
Alexandro Burgagni <alexan94dro@gmail.com>
2022-03-26 12:35:18 +0100

    Stabiliti modelli tramite interfaccia funzinoale
    Co-authored-by: DavisCacici <DavisCacici@users.noreply.github.com>

---
+++ models/models.ts
@@ -0,0 +1,31 @@
+export interface Course {
+    readonly id:number;
+    readonly name:string;
+    readonly state:string;
+    readonly description:string;
+};
+
+export interface Role{
+    readonly id:number;
+    readonly name:string;
+    readonly description:string;
+};
+
+export interface User{
+    readonly id:number;
+    readonly user_name:string;
+    readonly email:string;
+    readonly token:string;
+    readonly last_request:Date;
+    readonly role:Role;
+}
+export interface Step {
+    readonly step:string;
+    readonly description:string;
+};
+
+export interface Progres {
+    readonly step:Step;
+    readonly state:string;
+    readonly course_user_id:number;
+};
