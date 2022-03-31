6c25dd11f44686ccd9f951eb909b65cad97ac719
Alexandro Burgagni <alexan94dro@gmail.com>
2022-03-31 18:48:58 +0200

    Login component

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link as RouteLink } from 'react-router-dom';
+import { Link , Checkbox, FormControl, FormControlLabel,Button, Card, CardContent, Typography, CardActions, TextField, CardMedia} from '@mui/material'
+import LockIcon from '@mui/icons-material/Lock';
 
 interface LoginProps {
   readonly onLogin?: () => void;
@@ -7,14 +9,48 @@ interface LoginProps {
 export const Login: React.FC<LoginProps> = (props) => {
   const { onLogin } = props;
   return (
-    <div className="box">
-      <h2>Login!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-      <p>
-        <button onClick={onLogin}>LOGIN</button>
-      </p>
-    </div>
+      <Card sx={{ flexDirection: 'column', alignContent: 'space-between', display: 'inline-block', mx: '5px', transform: 'scale(0.9)', minWidth: 275, minHeight:400, borderRadius: '25px'  }}>
+            <CardContent>
+                <CardMedia
+                component='img'
+                height="125"
+                src = "logo192Er.png"
+                alt="Er Training Logo"
+                sx={{transform: 'scale(0.95)'}}
+                />
+                {/*<Typography sx={{ fontSize: 20 }} color="text.primary" gutterBottom>
+                BENVENUTO IN ER TRAINING
+                </Typography>*/}
+                
+                <FormControl margin="dense">
+                    <TextField
+                    required
+                    id="mail-required"
+                    label="Mail"
+                    type="email"
+                    sx={{mt: 2, mb:1}}
+                    />
+                <br/>
+                    <TextField
+                    required
+                    id="password-required"
+                    label="Password"
+                    type="password"
+                    sx={{mt: 1, mb:2}}
+                    />
+                    <div className='loginDiv'>
+                        <FormControlLabel control={<Checkbox />} label="Ricordami" />
+                        <LockIcon></LockIcon>
+                        <Link href="#">Recupera Password</Link>
+                    </div>
+                    </FormControl>
+                <CardActions sx={{display:"block", mx:1 }}>
+                    {/*<Button sx={{px:15}} variant="contained">Login</Button>*/}
+                    <Button sx={{px:15}} variant="contained" onClick={onLogin}>Login</Button>
+                </CardActions>
+                
+            </CardContent>
+            
+        </Card>
   );
 };
