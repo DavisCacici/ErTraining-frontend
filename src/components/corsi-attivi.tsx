002002d960fd0f0edf307b734a7bf18dab660ad6
melaniatizzi <melaniatizzi@gmail.com>
2022-03-31 13:33:04 +0200

    prove stile card Corsi attivi

--- src/components/corsi-attivi.tsx
+++ src/components/corsi-attivi.tsx
@@ -1,37 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
-import { createTheme } from '@mui/system';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { createTheme } from "@mui/system";
 
 const theme = createTheme({
   typography: {
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
+      "Arial",
+      "sans-serif",
+    ].join(","),
   },
 });
 
 export default function CorsiAttivi() {
-    return (
-      <div>
-        <Card sx={{ maxWidth: 0.6, position: 'relative' }}>
-          <CardActionArea>          
+  return (
+    <Card sx={{ maxWidth: 0.6, position: "relative", margin: 20 }}>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "bold" }}
+        >
+          Corsi attivi
+        </Typography>
+        
+        <Card sx={{ maxWidth: 0.9, maxHeight: 40}} style={{backgroundColor: "#E4F7FF", margin: 10}}>
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div" style={{fontSize: "bold"}}>
-                Corsi attivi
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lista corsi...
+              <Typography variant="body2" color="text.secondary" textAlign="left">
+                Corso Sicurezza rischio basso
               </Typography>
             </CardContent>
-          </CardActionArea>
         </Card>
-      </div>
-    );
-  }
+
+        <Card sx={{ maxWidth: 0.9, maxHeight: 40, }} style={{backgroundColor: "#E4F7FF", margin: 10}}>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary" textAlign="left">
+                Corso Sicurezza rischio alto
+              </Typography>
+            </CardContent>
+        </Card>
+
+      </CardContent>
+    </Card>
+  );
+}
