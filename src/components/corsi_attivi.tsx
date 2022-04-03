d3bad7512734a1f3d7410f1ccb5bd7dc2a754b01
melaniatizzi <melaniatizzi@gmail.com>
2022-04-03 21:25:14 +0200

    icone

--- src/components/corsi_attivi.tsx
+++ src/components/corsi_attivi.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 // MUI
-import { Box, Button, Card, CardActionArea, CardActions, CardContent, Grid, IconButton, SvgIcon, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Grid,
+  IconButton,
+  Stack,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
 import { createTheme } from "@mui/system";
 
 // CSS customizzato
-import './corsi-attivi.scss';
+import "./corsi-attivi.scss";
 
 // Icone
-import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
-import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
-import PlayCircleTwoToneIcon from '@mui/icons-material/PlayCircleTwoTone';
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
+import PlayCircleTwoToneIcon from "@mui/icons-material/PlayCircleTwoTone";
 
 const theme = createTheme({
   typography: {
@@ -25,10 +37,26 @@ const theme = createTheme({
   },
 });
 
-export const CorsiAttivi: React.FC= () =>  {
+const IconeAzioniCorso: React.FC = () => {
   return (
-    <div className='card-style'>
-      <Card sx={{ maxWidth: 1010, position: "relative"}}>
+    <Stack direction="row" spacing={1}>
+      <IconButton aria-label="play game">
+        <PlayCircleTwoToneIcon />
+      </IconButton>
+      <IconButton aria-label="edit">
+        <EditTwoToneIcon />
+      </IconButton>
+      <IconButton aria-label="delete">
+        <DeleteTwoToneIcon />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export const CorsiAttivi: React.FC = () => {
+  return (
+    <div className="card-style">
+      <Card sx={{ maxWidth: 1010, position: "relative" }}>
         <CardContent>
           <Typography
             gutterBottom
@@ -37,17 +65,28 @@ export const CorsiAttivi: React.FC= () =>  {
             sx={{ fontWeight: "bold" }}
           >
             <h5 className="card-title">Corsi attivi</h5>
-          </Typography>        
-        
-          <Card sx={{ maxWidth: 980, maxHeight: 40, padding: -2}} style={{backgroundColor: "#E4F7FF", margin: 10}}>  
-            <CardContent>                  
-              <Box>
-                <Typography sx={{ display: 'flex', alignItems: 'left', m: 0, p: 0 }} variant="body2" color="text.secondary"> 
-                  Corso sicurezza rischio basso                                
-                </Typography> 
-              </Box>  
+          </Typography>
 
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Card
+            sx={{ maxWidth: 980, maxHeight: 40, padding: -2 }}
+            style={{ backgroundColor: "#E4F7FF", margin: 10 }}
+          >
+            <CardContent>
+              <Box>
+                <Typography
+                  sx={{ display: "flex", alignItems: "left", m: 0, p: 0 }}
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  Corso sicurezza rischio basso
+                </Typography>
+              </Box>
+
+              <Box>
+                <IconeAzioniCorso></IconeAzioniCorso>
+              </Box>
+
+              {/* <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <IconButton edge={false} size="small" color="primary">                                       
                   <PlayCircleTwoToneIcon sx={{ mr: 2}}/>              
                 </IconButton>     
@@ -59,8 +98,8 @@ export const CorsiAttivi: React.FC= () =>  {
                 <IconButton edge={false} size="small" color="primary">
                   <DeleteTwoToneIcon sx={{ mr: 2}}/>                   
                 </IconButton>
-              </Box>                                   
-            </CardContent> 
+              </Box>                                    */}
+            </CardContent>
 
             {/* <CardActions disableSpacing>
               <IconButton aria-label="Vai ai giochi interattivi del corso">
@@ -75,17 +114,24 @@ export const CorsiAttivi: React.FC= () =>  {
             </CardActions>           */}
           </Card>
 
-          <Card sx={{ maxWidth: 980, maxHeight: 40, }} style={{backgroundColor: "#E4F7FF", margin: 10}}>
-            <CardActionArea>              
-                <CardContent>                  
-                  <Typography variant="body2" color="text.secondary" textAlign="left">
-                    Corso Sicurezza rischio alto                                       
-                  </Typography>                  
-                </CardContent>
-            </CardActionArea>            
+          <Card
+            sx={{ maxWidth: 980, maxHeight: 40 }}
+            style={{ backgroundColor: "#E4F7FF", margin: 10 }}
+          >
+            <CardActionArea>
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  textAlign="left"
+                >
+                  Corso Sicurezza rischio alto
+                </Typography>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </CardContent>
       </Card>
     </div>
   );
-}
+};
--- src/components/shortcut.tsx
+++ src/components/shortcut.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button, Card, CardActionArea, CardContent, createTheme, Typography } from '@mui/material';
-import './corsi-attivi.scss';
-import { flexbox } from '@mui/system';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  createTheme,
+  Typography,
+} from "@mui/material";
+import "./corsi-attivi.scss";
+import { flexbox } from "@mui/system";
 
 const theme = createTheme({
   typography: {
@@ -19,20 +26,39 @@ const theme = createTheme({
 });
 
 export default function ShortcutComponent() {
-    return (
-      <div className='card-style'>
-        <Card sx={{ maxWidth: 1010, position: 'relative'}}>       
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              <h5 className="card-title">Shortcut</h5>
-            </Typography>  
-            <Button sx={{ mt: 0, mb: 0, mr: 1, ml: 1, p:3, borderRadius: 4}} variant="contained">CORSI ATTIVI</Button> 
-            <Button sx={{ mt: 0, mb: 0, mr: 0, ml: 1, p:3, borderRadius: 4 }} variant="contained">CORSI CONCLUSI</Button>     
-            <Button sx={{ mt: 0, mb: 0, mr: 0, ml: 20, p:3, borderRadius: 4 }} variant="outlined">CREA CORSO</Button> 
-            <Button sx={{ mt: 0, mb: 0, mr: 0, ml: 2, p:3, borderRadius: 4 }} variant="contained">AGGIUNGI UTENTE</Button>     
-          </CardContent>
-        </Card>
-      </div>
-    );
-    
-  }
+  return (
+    <div className="card-style">
+      <Card sx={{ maxWidth: 1010, position: "relative" }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            <h5 className="card-title">Shortcut</h5>
+          </Typography>
+          <Button
+            sx={{ mt: 0, mb: 0, mr: 1, ml: 1, p: 3, borderRadius: 4 }}
+            variant="contained"
+          >
+            CORSI ATTIVI
+          </Button>
+          <Button
+            sx={{ mt: 0, mb: 0, mr: 0, ml: 1, p: 3, borderRadius: 4 }}
+            variant="contained"
+          >
+            CORSI CONCLUSI
+          </Button>
+          <Button
+            sx={{ mt: 0, mb: 0, mr: 0, ml: 20, p: 3, borderRadius: 4 }}
+            variant="outlined"
+          >
+            CREA CORSO
+          </Button>
+          <Button
+            sx={{ mt: 0, mb: 0, mr: 0, ml: 2, p: 3, borderRadius: 4 }}
+            variant="contained"
+          >
+            AGGIUNGI UTENTE
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
