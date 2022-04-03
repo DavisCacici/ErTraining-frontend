866b8326a63d1b063610933e56aacccb5cf8e75a
Alexandro Burgagni <alexan94dro@gmail.com>
2022-04-03 15:11:06 +0200

    login con chiamate api, logo risoluzione più alta

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,13 +1,24 @@
 import { Link as RouteLink } from 'react-router-dom';
 import { Link , Checkbox, FormControl, FormControlLabel,Button, Card, CardContent, Typography, CardActions, TextField, CardMedia} from '@mui/material'
 import LockIcon from '@mui/icons-material/Lock';
+import { useEffect, useState } from 'react';
+import { login } from '../apis/createPost';
 
 interface LoginProps {
-  readonly onLogin?: () => void;
+  readonly onLogin: (email: string, password:string) => void;
 }
 
 export const Login: React.FC<LoginProps> = (props) => {
   const { onLogin } = props;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  /*useEffect(()=>{login(email, password);}, []);*/
+  const handleEmail = (e: any) => {
+    setEmail(e.target.value);
+  }
+  const handlePassword = (e: any) => {
+    setPassword(e.target.value);
+  }
   return (
       <Card sx={{ flexDirection: 'column', alignContent: 'space-between', display: 'inline-block', mx: '5px', transform: 'scale(0.9)', minWidth: 275, minHeight:400, borderRadius: '25px'  }}>
             <CardContent>
@@ -28,6 +39,7 @@ export const Login: React.FC<LoginProps> = (props) => {
                     id="mail-required"
                     label="Mail"
                     type="email"
+                    onChange={handleEmail}
                     sx={{mt: 2, mb:1}}
                     />
                 <br/>
@@ -36,6 +48,7 @@ export const Login: React.FC<LoginProps> = (props) => {
                     id="password-required"
                     label="Password"
                     type="password"
+                    onChange={handlePassword}
                     sx={{mt: 1, mb:2}}
                     />
                     <div className='loginDiv'>
@@ -46,7 +59,7 @@ export const Login: React.FC<LoginProps> = (props) => {
                     </FormControl>
                 <CardActions sx={{display:"block", mx:1 }}>
                     {/*<Button sx={{px:15}} variant="contained">Login</Button>*/}
-                    <Button sx={{px:15}} variant="contained" onClick={onLogin}>Login</Button>
+                    <Button sx={{px:15}} variant="contained" onClick={()=>onLogin(email, password)}>Login</Button>
                 </CardActions>
                 
             </CardContent>
