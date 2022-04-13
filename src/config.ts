7009ba82ce815b04c1f76c64214e36cb82d1eaf0
Marco Villa <marco8villa6@gmail.com>
2022-04-13 15:14:09 +0200

    config server prod

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // NOTE: TEST DATA!!!
 
 // Dev:
-export const SERVER_URL = 'http://127.0.0.1:8000/api';
+// export const SERVER_URL = 'http://127.0.0.1:8000/api';
 // Test:
 
 // Prod:
+export const SERVER_URL = 'https://93.45.34.154/api';
