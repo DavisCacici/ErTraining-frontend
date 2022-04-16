fe60b9c413c4d027799a1a66df6808aef22d91cd
DavisCacici <antoninodaviscacici@gmail.com>
2022-04-16 10:52:06 +0200

    try comunication websocket

--- src/components/ProgressStudents.tsx
+++ src/components/ProgressStudents.tsx
@@ -1,13 +1,24 @@
+import { Box } from '@mui/material';
 import Pusher from 'pusher-js';
 import React from "react";
 
-export const progressStudent = () => {
-    var pusher = new Pusher('b25895c8fd0b48f0c6b1', {
+export const ProgressStudent = () => {
+    Pusher.logToConsole = true;
+    var pusher = new Pusher('cb6d3938abbc2b5ba712', {
         cluster: 'eu'
     });
 
     var channel = pusher.subscribe('setProgress');
     channel.bind('changeProgress', function(data: any){
-        alert(JSON.stringify(data));
-    })
+        console.log(data);
+        // alert(JSON.stringify(data));
+    });
+
+    return (
+        <Box>
+            <h1>
+                Prova
+            </h1>
+        </Box>
+    )
 };
