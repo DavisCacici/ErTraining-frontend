44dbde5d2bdb59b9a4d8ec19401d8342f725d5f8
Alexandro Burgagni <alexan94dro@gmail.com>
2022-04-16 10:18:32 +0200

    Creato comnente anagrafica content, WIP api call

---
+++ src/components/anagraficheContent.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { Box, Stack, IconButton} from '@mui/material'
+import {Table, TableBody, TableCell, TableHead, TableContainer, TableRow, TablePagination } from '@mui/material';
+import { User } from '../models/models'
+
+//icone
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+
+
+interface AnagraficheContentProps {
+    readonly tableData:User[];
+    readonly type:string;
+    readonly search:string|undefined;
+};
+
+
+export const AnagraficaContent:React.FunctionComponent<AnagraficheContentProps> = (props) => {
+
+    //Hooks
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    
+  
+    //Handler
+    const handleChangePage = (event: unknown, newPage: number) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+
+    //const deleteRequest = (id:number) =>{
+      //deleteUser(id).then(() => {decideCalls(props.type)}).catch((e)=>{console.log(e); decideCalls(props.type);})
+    //} 
+
+    const richiediRicerca = () =>{
+        if(filterIfRequired(props.search)){
+          const filteredRows = props.tableData.filter((row) => {
+            return row.user_name.toLowerCase().includes((props.search as string).toLocaleLowerCase());
+            });
+          if(filteredRows !== props.tableData){
+            return filteredRows;
+          }else{
+            return props.tableData;
+          }  
+        }else{
+          return props.tableData;
+        }
+        
+    };
+    
+    function filterIfRequired(key:string|undefined):boolean{
+        return typeof(key) === 'string'? true: false;
+    }
+
+    return(
+    <Box>
+        <TableContainer>
+            <Table sx={{minWidth: 400, px:5}}>
+                <TableHead>
+                    <TableRow >
+                        <TableCell align='left'> ID </TableCell>
+                        <TableCell align="left"> User_Name </TableCell>
+                        <TableCell align="right"> Azioni </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {richiediRicerca().map((row) => (
+                    <TableRow
+                        key={row.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell >{row.id}</TableCell>
+                        <TableCell align="left">
+                            {row.user_name}
+                        </TableCell>
+                        <TableCell align="right">
+                          <Stack direction='row' justifyContent='right' spacing={1}>
+                            <IconButton onClick={ () => {console.log('Edit Button')}} aria-label="edit">
+                              <EditTwoToneIcon color="primary"/>
+                            </IconButton>
+                            <IconButton onClick={ () => {console.log('Delete Button')}} aria-label="delete">
+                              <DeleteTwoToneIcon color="primary"/>
+                            </IconButton>
+                          </Stack>
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={props.tableData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}/>       
+    </Box>
+    )
+};
--- src/components/angrafiche.tsx
+++ src/components/angrafiche.tsx
@@ -1,26 +1,15 @@
-import { Box, Select, MenuItem, Stack, IconButton, Card, Typography, InputAdornment, TextField, FormControl} from '@mui/material'
-import {Table, TableBody, TableCell, TableHead, TableContainer, TableRow, TablePagination } from '@mui/material';
-import { useState, useEffect } from 'react';
+import { Box,Typography, InputAdornment, TextField, Card, IconButton, Tabs, Tab} from '@mui/material'
 import { User } from '../models/models'
-import {teachersList, studentsList, deleteUser} from '../apis/tutor_call'
+import { teachersList, studentsList, deleteUser } from '../apis/tutor_call'
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { AnagraficaContent } from './anagraficheContent';
 // Icone
-import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
-import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import AddCircleTwoToneIcon from '@mui/icons-material/AddCircleTwoTone';
+import CoPresentTwoToneIcon from '@mui/icons-material/CoPresentTwoTone';
+import SchoolTwoToneIcon from '@mui/icons-material/SchoolTwoTone';
 
-const IconeAzioniAngarfica: React.FC = () => {
-  return (
-    <Stack direction="row" justifyContent="right" spacing={1}>
-      <IconButton aria-label="edit">
-        <EditTwoToneIcon color="primary" />
-      </IconButton>
-      <IconButton aria-label="delete">
-        <DeleteTwoToneIcon color="primary" />
-      </IconButton>
-    </Stack>
-  );
-};
 
 interface AnagraficheProps {
     readonly defaultType:string;
@@ -38,22 +27,21 @@ export const Anagrafiche: React.FunctionComponent<AnagraficheProps> = (props) =>
     function decideCalls(type:string){
       if(type === 'teacher'){
         teachersList().then((response)=> {console.log(response)})
-        .catch((error)=>{console.log(error); Data = [user1,user2]});
+        .catch((error)=>{console.log(error); Data = [user1,user2]; setDataEnd(Data);});
       }
       else if(type === 'student')
       {
         studentsList().then((response)=> {console.log(response)})
-        .catch((error)=>{console.log(error); Data = [user1,user2]});
+        .catch((error)=>{console.log(error); Data = [user1,user2]; setDataEnd(Data)});
       }else{
         console.log("Questo è un problema");
       }
     };
-    
-    useEffect(() => {
-      decideCalls(type)
-    } )
-    
-    
+
+    const refershApi = useCallback(
+      (type) => {
+        decideCalls(type);
+      }, []) 
 
   //Mock-up Data
 
@@ -71,105 +59,41 @@ export const Anagrafiche: React.FunctionComponent<AnagraficheProps> = (props) =>
     role: 'teacher',
   };
 
-  Data = [user1, user2];
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setType(newValue);
+    refershApi(newValue);
+  };
 
-    //Hooks
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-    const [righeCorrenti, setRigheCorrenti] = useState<User[]>(Data);
-    
+  const [DataEnd, setDataEnd] = useState<User[]>([]);
+
+  const [chiaveDiRicerca, setchiaveDiRicerca] = useState<string|undefined>()
+
+
   
-    //Handler
-    const handleChangePage = (event: unknown, newPage: number) => {
-      setPage(newPage);
-    };
-  
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setRowsPerPage(parseInt(event.target.value, 10));
-      setPage(0);
-    };
 
-    const richiediRicerca = (newValChange: string) => {
-      const filteredRows = Data.filter((row) => {
-        return row.user_name.toLowerCase().includes(newValChange.toLowerCase());
-      });
-      setRigheCorrenti(filteredRows);
-    };
-
-    const deleteRequest = (id:number) =>{
-      deleteUser(id).then(() => {decideCalls(type)}).catch((e)=>{console.log(e); decideCalls(type);})
-    }
-    
-    return (<Box>
+    return (
+    <Box>
         <Box display='flex' justifyContent='space-between' alignItems='end' sx={{ pb: 5 }}>
-            <Typography textAlign='left' variant="h5" sx={{ fontWeight: "bold" }}>Anagrafica {type} </Typography>
+            <Typography textAlign='left' variant="h5" sx={{ fontWeight: "bold" }}>Anagrafiche</Typography>
             <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <TextField id="input-searchbar" label="Cerca..." variant="outlined" onChange={(e)=>{richiediRicerca(e.target.value.toString())}} InputProps={{startAdornment: (<InputAdornment position="start">
+                <TextField id="input-searchbar" label="Cerca..." variant="outlined" onChange={(e)=>{setchiaveDiRicerca(e.target.value.toString()) ; console.log(chiaveDiRicerca);}} InputProps={{startAdornment: (<InputAdornment position="start">
                   <SearchTwoToneIcon fontSize='medium'></SearchTwoToneIcon>
                 </InputAdornment>
               )}}/>
             </Box>
         </Box>
-        <Card>
-            <Box display='flex' justifyContent='space-between' alignItems='center' sx={{ mx:2, my:1 }}>
-              <FormControl>
-                <Select
-                  defaultValue={type}
-                  labelId="role-label"
-                  id="role"
-                  label="Role"
-                  onChange={(e)=>{setType(e.target.value)}}
-                >
-                  <MenuItem value={'student'}>Student</MenuItem>
-                  <MenuItem value={'teacher'}>Teacher</MenuItem>
-                </Select>
-              </FormControl>
-              <IconButton color='primary' aria-label='Add' onClick={()=>{props.routeCallback(type)}}>
-                  <AddCircleTwoToneIcon fontSize='large' ></AddCircleTwoToneIcon>
-              </IconButton>
-            </Box>
-            <TableContainer>
-                <Table sx={{minWidth: 400, px:5}}>
-                    <TableHead>
-                        <TableRow >
-                            <TableCell align='left'> ID </TableCell>
-                            <TableCell align="left"> User_Name </TableCell>
-                            <TableCell align="right"> Azioni </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {righeCorrenti.map((row) => (
-                        <TableRow
-                            key={row.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell >{row.id}</TableCell>
-                            <TableCell align="left">
-                                {row.user_name}
-                            </TableCell>
-                            <TableCell align="right">
-                              <Stack direction='row' justifyContent='right' spacing={1}>
-                                <IconButton onClick={ () => {console.log('Edit Button')}} aria-label="edit">
-                                  <EditTwoToneIcon color="primary"/>
-                                </IconButton>
-                                <IconButton onClick={ () => {deleteRequest(row.id)}} aria-label="delete">
-                                  <DeleteTwoToneIcon color="primary"/>
-                                </IconButton>
-                              </Stack>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={Data.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}/>       
-        </Card>
+      <Card>
+        <Box display='flex' justifyContent='space-between' alignItems='center' sx={{ mx:2, my:1 }}>
+          <Tabs value={type} onChange={handleChange} aria-label="icon label tabs example">
+            <Tab value={'student'} icon={<SchoolTwoToneIcon />} label="STUDENTS" />
+            <Tab value={'teacher'} icon={<CoPresentTwoToneIcon />} label="TEACHERS" />
+          </Tabs>
+          <IconButton color='primary' aria-label='Add' onClick={()=>{console.log('route to Add User')}}>
+              <AddCircleTwoToneIcon fontSize='large' ></AddCircleTwoToneIcon>
+          </IconButton>
+        </Box>
+          <AnagraficaContent search={chiaveDiRicerca} tableData={DataEnd} type={type}></AnagraficaContent>
+      </Card>
     </Box>
   );
 };
