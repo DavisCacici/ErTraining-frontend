1186cfed55e7ebdf5e3886d4b8f67a5b0757f1ae
leogaru <bho1234321@gmail.com>
2022-04-21 16:05:37 +0200

    ok

--- src/components/courses-list.tsx
+++ src/components/courses-list.tsx
@@ -201,3 +201,202 @@ export const CoursesList: React.FC<CoursesListProps> = (props) => {
     </div>
   );
 };
+
+interface Props {
+  readonly course?: Course;
+}
+
+export const CompletedCourses: React.FC<Props> = (props) => {
+
+ const [loading, setLoading] = useState(false);
+ const [completedCourses, setCompletedCourses] = useState<Course[]>([])
+ const [error, setError] = useState();
+
+ useEffect(() => {
+  setLoading(true);
+  coursesList()
+    .then((res) => setCompletedCourses(res.data.data as Course[]))
+    .catch((error: any) => setError(error))
+    .finally(() => {
+      setLoading(false);
+    });
+}, []);
+var courses: Course[] = [];
+if (completedCourses.length > 0){
+   for(var i =0; i<completedCourses.length; i++){
+     if (completedCourses[i].state === "Completato"){
+       courses.push(completedCourses[i])
+     }
+   }
+}
+
+
+if (loading)
+{return <p>Data is loading...</p>;}
+
+return (
+<Box>
+    <Typography textAlign="left" variant="h5" sx={{ fontWeight: 'bold' }}>
+      Gestione profilo
+    </Typography>
+    <Grid
+      container
+      direction="column"
+      alignItems="stretch"
+      justifyContent="center"
+      style={{ minHeight: '40vh' }}
+    >
+      <Grid item>
+          
+          <div className="card-style">
+    <Card sx={{ maxWidth: 1010, position: "relative" }}>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "bold" }}
+        >
+          <h5 className="card-title">Corsi Finiti</h5>
+        </Typography>
+
+        <List
+          style={{
+            backgroundColor: "#E4F7FF",
+            margin: 10,
+            borderRadius: "5px",
+          }}
+        >
+          {courses.map((course) => (
+            <ListItemButton
+              key={course.id}
+              sx={{
+                minHeight: 15,
+                justifyContent: "center",
+                px: 1,
+              }}
+
+            >
+
+              <ListItemText
+                key={course.name}
+                primary={course.name}
+                sx={{ ml: 2, opacity: 1, justifyContent: "center", }}
+              />
+              <ListItemText
+                key={course.description}
+                primary={course.description}
+                sx={{ ml: 2, opacity: 1, justifyContent: "center", }}
+              />
+              
+            </ListItemButton>
+          ))}
+        </List>
+      </CardContent>
+    </Card>
+  </div>
+      </Grid>    
+    </Grid>
+  </Box>
+  );
+};
+
+interface Props {
+  readonly course?: Course;
+}
+
+export const ActiveCourses: React.FC<Props> = (props) => {
+
+ const [loading, setLoading] = useState(false);
+ const [activeCourses, setActiveCourses] = useState<Course[]>([])
+ const [error, setError] = useState();
+
+ useEffect(() => {
+  setLoading(true);
+  coursesList()
+    .then((res) => setActiveCourses(res.data.data as Course[]))
+    .catch((error: any) => setError(error))
+    .finally(() => {
+      setLoading(false);
+    });
+}, []);
+var courses: Course[] = [];
+if (activeCourses.length > 0){
+   for(var i =0; i<activeCourses.length; i++){
+     if (activeCourses[i].state === "Attivo"){
+       courses.push(activeCourses[i])
+     }
+   }
+}
+
+
+if (loading)
+{return <p>Data is loading...</p>;}
+
+return (
+<Box>
+    <Typography textAlign="left" variant="h5" sx={{ fontWeight: 'bold' }}>
+      Gestione profilo
+    </Typography>
+    <Grid
+      container
+      direction="column"
+      alignItems="stretch"
+      justifyContent="center"
+      style={{ minHeight: '40vh' }}
+    >
+      <Grid item>
+          
+          <div className="card-style">
+    <Card sx={{ maxWidth: 1010, position: "relative" }}>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "bold" }}
+        >
+          <h5 className="card-title">Corsi Attivi</h5>
+        </Typography>
+
+        <List
+          style={{
+            backgroundColor: "#E4F7FF",
+            margin: 10,
+            borderRadius: "5px",
+          }}
+        >
+          {courses.map((course) => (
+            <ListItemButton
+              key={course.id}
+              sx={{
+                minHeight: 15,
+                justifyContent: "center",
+                px: 1,
+              }}
+
+            >
+
+              <ListItemText
+                key={course.name}
+                primary={course.name}
+                sx={{ ml: 2, opacity: 1, justifyContent: "center", }}
+              />
+              <ListItemText
+                key={course.description}
+                primary={course.description}
+                sx={{ ml: 2, opacity: 1, justifyContent: "center", }}
+              />
+              
+            </ListItemButton>
+          ))}
+        </List>
+      </CardContent>
+    </Card>
+  </div>
+      </Grid>    
+    </Grid>
+  </Box>
+  );
+};
+
